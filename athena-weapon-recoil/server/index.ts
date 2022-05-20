b6d70cdadd9628e7c686327c0a39a54699549711
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2022-05-20 08:00:44 +0200

    Update index.ts

--- athena-weapon-recoil/server/index.ts
+++ athena-weapon-recoil/server/index.ts
@@ -2,7 +2,7 @@
 import * as alt from 'alt-server';
 import { PluginSystem } from '../../../server/systems/plugins';
 
-const PLUGIN_NAME = 'CORE ==> Athena Weapon Recoil';
+const PLUGIN_NAME = '[PLUGIN] ==> Athena Weapon Recoil';
 
 PluginSystem.registerPlugin(PLUGIN_NAME, () => {
     alt.log(`~lg~${PLUGIN_NAME} was Loaded`);
